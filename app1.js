34265a91d50ec32ffd16ecffd2c18266f4948440
sachpreet71 <sachpreet71@gmail.com>
2024-05-02 00:06:11 +0530

    api to work

--- app1.js
+++ app1.js
@@ -6,9 +6,31 @@ const button = document.querySelector('#btn-translate');
 const textInput = document.querySelector('.text-input');
 const output = document.querySelector('#output');
 
+var serverURL = "	https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+}
 
 
 
-button.addEventListener('click', () => {
-  output.innerHTML = "jnjfnsjnf " + textInput.value;
-});
+
+function clickHandler() {
+  // output.innerHTML = "jnjfnsjnf" + textInput.value;
+  var inputText = textInput.value;
+
+  // calling server
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      output.innerText = translatedText;
+    })
+    .catch(errorHandler);
+};
+
+button.addEventListener("click", clickHandler)
